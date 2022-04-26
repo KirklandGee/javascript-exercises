de0fd284afa84c0a7cccd67811171e117b65ec9f
Kirkland Gee <kirklandgee12@gmail.com>
2022-04-26 10:52:53 -0400

    solve problem 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  let celsisusTemp = (temp -32) * 5/9
+  return parseFloat(celsisusTemp.toFixed(1))
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  let farenheightTemp = (temp * 9/5) + 32
+  return parseFloat(farenheightTemp.toFixed(1))
 };
 
 // Do not edit below this line
