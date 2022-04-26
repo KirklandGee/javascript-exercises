404a9b5acff154f607d2bc49aa5dd8e43735627a
Kirkland Gee <kirklandgee12@gmail.com>
2022-04-26 10:42:42 -0400

    solve problem 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,26 @@
-const sumAll = function() {
-
+/* Take number X and add it to number X+1. Make that number curr.
+Add number curr to X+2, make that curr
+Add number curr to X+3...
+Return
+*/
+const sumAll = function(num1, num2) {
+    let finalSum = 0
+    if (num1 < 0 || num2 < 0 || typeof(num1) != 'number' || typeof(num2) != 'number') {
+        return "ERROR"
+    }
+    if (num1 < num2){
+    for (i=num1; i<=num2; i++) { 
+        finalSum += i
+        }
+    } else {
+    for (i=num2; i<=num1; i++) { 
+        finalSum += i
+        }
+    }
+    return finalSum
 };
+
+sumAll(1, 4)
 
 // Do not edit below this line
 module.exports = sumAll;
