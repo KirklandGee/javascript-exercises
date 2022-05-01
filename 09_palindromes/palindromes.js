84652395c7596f8bec52c24b9b8470e3c7289c8d
Kirkland Gee <kirklandgee12@gmail.com>
2022-05-01 10:02:01 -0400

    solve problem set 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
+//
 
+
+const palindromes = function (string) {
+    const re = /[\W_]/g;
+    const lowerCaseString = string.toLowerCase().replace(re, '')
+    const reverseString = lowerCaseString.split('').reverse().join('')
+    console.log(lowerCaseString)
+    console.log(reverseString)
+    return lowerCaseString === reverseString;
 };
+
+palindromes('bobs')
 
 // Do not edit below this line
 module.exports = palindromes;
