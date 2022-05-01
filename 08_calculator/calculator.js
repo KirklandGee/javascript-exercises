91afc98b6f1d5a03702a2f3165c775f465eaf9d5
Kirkland Gee <kirklandgee12@gmail.com>
2022-05-01 09:55:14 -0400

    solve problem set 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,49 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a +b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let output = 0
+  for (let item in array) {
+    output += parseInt(array[item]);
+ } 
+ return output
 };
 
-const multiply = function() {
+sum([7, 11])
 
+const multiply = function(array) {
+  let sum = 1
+  for (item in array) {
+    sum *= array[item];
+ } 
+ return sum
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  let multiplier = a
+  for (i=1; i<b; i++) {
+    multiplier *= a
+  }
+  return multiplier
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num == 0) {
+    return 1
+  }
+  for (i=num; i>2; i--) {
+    num *= (i-1)
+    console.log(num)
+  }
+  return num
 };
+
+factorial(10)
 
 // Do not edit below this line
 module.exports = {
